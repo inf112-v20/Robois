987bc377ff51fef1c6f92265f681c7beb1597adf
Thomas <thomas.storli@live.no>
2020-02-03 14:26:58 +0100

    Added some basic tests and implemented them in Board through TDD.

--- src/main/java/inf112/skeleton/app/objects/Board.java
+++ src/main/java/inf112/skeleton/app/objects/Board.java
@@ -1,7 +1,42 @@
 package inf112.skeleton.app.objects;
 
-class Board {
+import inf112.skeleton.app.objects.interfaces.IDrawable;
+
+public class Board {
+
+    private IDrawable[][] board;
+    private int height;
+    private int width;
+
     public Board() {
-        
+        this.width = 12;
+        this.height = 12;
+        this.board = new IDrawable[12][12];
+        this.fillBoard();
     }
+
+    private void fillBoard() {
+        for (int i = 0; i < this.height; i++){
+            for (int j = 0; j < this.width; j++){
+                this.board[i][j] = new IDrawable(){
+                    @Override
+                    public String getImage() {
+                        return null;
+                    }
+                };
+            }
+        }
+    }
+
+	public Integer getHeight() {
+		return this.height;
+	}
+
+	public Integer getWidth() {
+		return this.width;
+	}
+
+	public IDrawable getPosition(int i, int j) {
+		return this.board[i][j];
+	}
 }
---
+++ src/main/java/inf112/skeleton/app/objects/tiles/Gear.java
@@ -0,0 +1,15 @@
+package inf112.skeleton.app.objects.tiles;
+
+import inf112.skeleton.app.objects.interfaces.IDrawable;
+
+class Gear implements IDrawable{
+    public Gear() {
+        
+    }
+
+    @Override
+    public String getImage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}

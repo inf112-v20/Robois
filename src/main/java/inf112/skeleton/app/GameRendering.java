dceab9c82fd7e0179cb3441a31522ddf8c95d864
kasperdt <45585490+kasperdt@users.noreply.github.com>
2020-05-08 12:21:20 +0200

    changed card layout

--- src/main/java/inf112/skeleton/app/GameRendering.java
+++ src/main/java/inf112/skeleton/app/GameRendering.java
@@ -60,10 +60,10 @@ public class GameRendering {
         
         mainGamePanel.addObject(new InformationDisplay(150, 100, 100, 100, game));
         
-        ProgramCardLocked l = new ProgramCardLocked(900, 20, 500, 155);
+        ProgramCardLocked l = new ProgramCardLocked(904, 119, 500, 155);
         mainGamePanel.addObject(l);
 
-        ProgramCardHand h = new ProgramCardHand(1000, 300, 300, 500, game, l);
+        ProgramCardHand h = new ProgramCardHand(1010, 335, 300, 500, game, l);
         mainGamePanel.addObject(h);
 
         l.setHand(h);
